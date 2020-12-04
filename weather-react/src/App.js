a378700e6af7130bb9da50c57dded812bd85963e
carlton anand <carlton.anand@gmail.com>
2020-12-04 17:27:59 +0000

    adding basic css, pics

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -1,7 +1,22 @@
+import React from 'react';
+
+const api = {
+  key: "c0f22d45c9850b5eff55c8e331662738",
+  base: "https://api.openweathermap.org/data/2.5"
+}
+
 function App() {
   return (
-    <div className="App">
-        Hello
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+          />
+        </div>
+      </main>
     </div>
   );
 }
