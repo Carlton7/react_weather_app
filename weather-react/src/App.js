ef4bb02c5299e88719573458f388bdb5f3ebfb99
carlton anand <carlton.anand@gmail.com>
2020-12-16 16:49:50 +0000

    mvp complete. app diplays basic weather info when a query for a place is put in. changes include setting the temp, description and background depending on the temp

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -36,7 +36,7 @@ function App() {
   }
 
   return (
-    <div className="app">
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
         <div className="search-box">
           <input
@@ -48,18 +48,22 @@ function App() {
             onKeyPress={search}
           />
         </div>
-        <div className="weather-info">
-          <div className="location-box">
-            <div className="location"> New York City, US</div>
-            <div className="date">{dateBuilder(new Date())}</div>
-          </div>
-          <div className="weather-box">
-            <div className="temp">
-              15°c
+        {(typeof weather.main !="undefined") ? ( 
+        <div>
+          <div className="weather-info">
+            <div className="location-box">
+              <div className="location">{weather.name}, {weather.sys.country}</div>
+              <div className="date">{dateBuilder(new Date())}</div>
             </div>
-            <div className="weather">Sunny</div>
+            <div className="weather-box">
+              <div className="temp">
+                {Math.round(weather.main.temp)}°C
+              </div>
+              <div className="weather">{weather.weather[0].main}</div>
           </div>
         </div>
+        </div>
+        ) : ('')}
       </main>
     </div>
   );
