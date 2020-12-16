cfa55e5f573082889c426593308dbe9f0042130b
carlton anand <carlton.anand@gmail.com>
2020-12-16 16:25:35 +0000

    adding search logic for weather by place name

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const api = {
   key: "c0f22d45c9850b5eff55c8e331662738",
-  base: "https://api.openweathermap.org/data/2.5"
+  base: "https://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState('');
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+        console.log(result);
+      });  
+    }
+  }
 
   const dateBuilder = (d) => {
     let months = ["January", "February","March","April","May",
@@ -29,6 +43,9 @@ function App() {
             type="text"
             className="search-bar"
             placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
           />
         </div>
         <div className="weather-info">
